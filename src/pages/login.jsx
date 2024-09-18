b722880e1c7b6b11f5478324a96a8776a86dd225
Moiz Ali <abdulmoizali321@gmail.com>
2024-09-18 10:21:07 +0500

    push

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -38,7 +38,8 @@ function Login() {
         <>
             <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img className="mx-auto h-10 w-auto" src="src/assets/todo.512x512.png" alt="Your Company" />
+                    <img className="mx-auto h-10 w-auto" src="https://i.ibb.co/RbVPLgH/todo.png" alt="todo" border="0" />
+                    {/* <img className="mx-auto h-10 w-auto" src="src/assets/todo.512x512.png" alt="Your Company" /> */}
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Log In To Your Account</h2>
                 </div>
 
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -45,7 +45,9 @@ function Signup() {
     <>
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img className="mx-auto h-10 w-auto" src="src/assets/todo.512x512.png" alt="Your Company" />
+          <img className="mx-auto h-10 w-auto" src="https://i.ibb.co/RbVPLgH/todo.png" alt="todo" border="0" />
+
+          {/* <img className="mx-auto h-10 w-auto" src="src/assets/todo.512x512.png" alt="Your Company" /> */}
           <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Create An Account</h2>
         </div>
 
