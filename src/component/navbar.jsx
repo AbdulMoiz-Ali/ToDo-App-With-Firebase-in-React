030688465f9e6e82c782fac6e6d6a7c2b8148651
Moiz Ali <abdulmoizali321@gmail.com>
2024-09-18 10:06:02 +0500

    push

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -111,7 +111,7 @@ function Navbar() {
                                     <button onClick={toggleMenu} type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                         <span className="absolute -inset-1.5"></span>
                                         <span className="sr-only">Open user menu</span>
-                                        <img className="h-8 w-8 rounded-full" src="public/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png" alt="" />
+                                        <img className="h-8 w-8 rounded-full" src="./public/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png" alt="" />
                                     </button>
                                 </div>
 
